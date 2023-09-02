9cba495c4a904d207dbce47930d71303c55a921d
tsuzukia21 <toshitake.s@outlook.jp>
2023-09-02 11:31:48 +0900

    2023/09/02

--- st_mito.py
+++ st_mito.py
@@ -1,18 +1,19 @@
-import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
 from mitosheet.streamlit.v1 import spreadsheet
+import streamlit as st
+import pandas as pd
 
-st.header("MitoSheet Demo")
+if not hasattr(st.session_state, "df_mito"):
+    st.session_state["df_mito"]= pd.DataFrame()
 
-df = sns.load_dataset('tips')
+st.header("mitoを使ったデータ分析")
+st.write("***mitoを使ったデータ分析が出来ます。お好きなファイルをアップロードしてください。***")
+upload_file = st.file_uploader('分析したいファイルを選択してください', type=['csv', 'xlsx'])
+if upload_file is not None:
+    if upload_file.name.endswith("csv"):
+        st.session_state.df_mito=pd.read_csv(upload_file)
+    elif upload_file.name.endswith("xlsx"):
+        st.session_state.df_mito=pd.read_excel(upload_file)
+    final_df,code=spreadsheet(st.session_state.df_mito)
 
-# Display the dataframe in a Mito spreadsheet
-final_dfs, code = spreadsheet(df)
-
-# Display the final dataframes created by editing the Mito component
-# This is a dictionary from dataframe name -> dataframe
-st.write(final_dfs)
-
-# Display the code that corresponds to the script
-st.code(code)
+    st.write(final_df)
+    st.code(code)
