e8e8bd76bdeb53b2c5bc2bd6d0cea09fe1f86b0b
tsuzukia21 <toshitake.s@outlook.jp>
2024-02-03 00:25:31 +0900

    agent_update

--- st_chat_Agent.py
+++ st_chat_Agent.py
@@ -1,34 +1,31 @@
 import streamlit as st
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.chat_models import ChatOpenAI
+from langchain.agents import AgentType, initialize_agent, Tool
 from langchain.callbacks import StreamlitCallbackHandler
-from langchain.tools import DuckDuckGoSearchRun
-from langchain.chat_models import ChatOpenAI
-from langchain.agents import AgentType, initialize_agent, load_tools,Tool
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain import LLMMathChain
+from langchain.chains import LLMMathChain
 from langchain.memory import ConversationBufferMemory
 from langchain.memory import StreamlitChatMessageHistory
 from langchain.prompts import MessagesPlaceholder
 from langchain.schema.messages import SystemMessage
-import openai
 import streamlit_antd_components as sac
+from langchain_core.runnables import RunnableConfig
+import os
 
 def agent():
     st.title("Agent by Streamlit")
-
-    if st.session_state.openai_api_key == "":
-        sac.alert(label='warning', description='Please add your OpenAI API key to continue.', color='red', banner=[False, True], icon=True, size='lg')
-        st.stop()
-    
+  
     search = DuckDuckGoSearchRun()
 
-    attrs=["messages_agent","agent_kwargs"]
+    attrs=["messages_agent","kwargs_agent"]
     for attr in attrs:
         if attr not in st.session_state:
             st.session_state[attr] = []
-    if "Clear" not in st.session_state:
-        st.session_state.Clear = False
+    if "Clear_agent" not in st.session_state:
+        st.session_state.Clear_agent = False
 
-    agent_kwargs = {
+    kwargs_agent = {
         "system_message": SystemMessage(content="You are an AI chatbot having a conversation with a human.", additional_kwargs={}),
         "extra_prompt_messages": [MessagesPlaceholder(variable_name="history")],
     }
@@ -49,33 +46,55 @@ def agent():
             description="useful for when you need to answer questions about math"
         ),
     ]
-    agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, agent_kwargs=agent_kwargs,verbose=False,memory=memory)
 
-    # Display chat messages_agent from history on app rerun
+    agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, kwargs=kwargs_agent,verbose=False,memory=memory)
+
     for message in st.session_state.messages_agent:
         if not message["role"]=="system":
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"],unsafe_allow_html=True)
+            if message["role"]=="user":
+                with st.chat_message(message["role"], avatar = "😊"):
+                    st.markdown(message["content"])
+            elif message["role"]=="assistant":
+                with st.chat_message(message["role"], avatar = "👩‍🎓"):
+                    st.markdown(message["content"])
 
     if user_prompt := st.chat_input("Send a message"):
+        if st.session_state.openai_api_key == "":
+            sac.alert(label='warning', description='Please add your OpenAI API key to continue.', color='red', banner=[False, True], icon=True, size='lg')
+            st.stop()
+        
         st.session_state.messages_agent.append({"role": "user", "content": user_prompt})
-        st.chat_message("user").write(user_prompt)
-        with st.chat_message("assistant"):
+        with st.chat_message("user", avatar = "😊"):
+            st.markdown(user_prompt)
+        
+        with st.chat_message("assistant" , avatar = "👩‍🎓"):
             st_callback = StreamlitCallbackHandler(st.container())
-            response = agent.run(user_prompt, callbacks=[st_callback])
+            cfg = RunnableConfig()
+            cfg["callbacks"] = [st_callback]
+            response = agent.invoke(user_prompt, cfg)
+            response = response["output"]
             st.write(response)
         st.session_state.messages_agent.append({"role": "assistant", "content": response})
-        st.session_state.Clear = True # チャット履歴のクリアボタンを有効にする
+        st.session_state.Clear_agent = True
 
-    # チャット履歴をクリアするボタンが押されたら、メッセージをリセット
-    if st.session_state.Clear:
+    if st.session_state.Clear_agent:
         if st.button('clear chat history'):
-            st.session_state.messages_agent = [] # メッセージのリセット
+            st.session_state.messages_agent = []
             response = ""
             msgs.clear()
             memory.clear()
-            st.session_state.Clear = False # クリア状態をリセット
-            st.experimental_rerun() # 画面を更新
+            st.session_state.Clear_agent = False
+            st.rerun()
 
 if __name__ == "__main__":
+    if not hasattr(st.session_state, "openai_api_key"):
+        try:
+            st.session_state.openai_api_key = os.environ["OPENAI_API_KEY"]
+        except:
+            st.session_state.openai_api_key = ""
+    with st.sidebar:
+        openai_api_key = st.text_input("OpenAI API Key", type="password")
+        if not openai_api_key == "":
+            st.session_state.openai_api_key = openai_api_key
+        st.write("if you are running the app locally,  \nthere is no need to enter the key  \nif it is already set as an environment variable.")
     agent()
