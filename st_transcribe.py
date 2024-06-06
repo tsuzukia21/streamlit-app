413befdad5c23afb7a38b97094eef39bf926812f
tsuzukia21 <tsuzukia21@users.noreply.github.com>
2024-06-06 10:56:33 +0900

    add transcribe app

--- st_transcribe.py
+++ st_transcribe.py
@@ -1,63 +1,30 @@
-import openai
+from openai import OpenAI
 import streamlit as st
-from typing import Any
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 import streamlit_antd_components as sac
 
-class JapaneseCharacterTextSplitter(RecursiveCharacterTextSplitter):
-    def __init__(self, **kwargs: Any):
-        separators = ["\n\n", "\n", "。", "、", " ", ""]
-        super().__init__(separators=separators, **kwargs)
-
-japanese_spliter = JapaneseCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=0,
-)
-
 def transcribe():
-    st.title("Transcribe by Streamlit") # タイトルの設定
+    st.title("Transcribe by Streamlit")
     if st.session_state.openai_api_key == "":
         sac.alert(label='warning', description='Please add your OpenAI API key to continue.', color='red', banner=[False, True], icon=True, size='lg')
         st.stop()
+    client = OpenAI(api_key=st.session_state.openai_api_key)
 
-    upload_file = st.file_uploader('音声文字起こしするファイルを選択してください  \nAPIの上限により25MB以上のファイルは文字起こし不可です。\
-                                ファイルを分割する等容量を少なくしてください', type=['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav'])
+    upload_file = st.file_uploader('Please select the file you want to transcribe. \nDue to API limitations, files over 25MB cannot be transcribed.\
+                                Please reduce the file size by splitting it or similar methods.', type=['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav'])
     if upload_file is not None:
-        st.subheader('ファイル詳細')
+        st.subheader('File Details')
         file_details = {'FileName': upload_file.name, 'FileType': upload_file.type, 'FileSize': upload_file.size}
         st.write(file_details)
-        file_name=upload_file.name.split('.')[0]
         if upload_file.size > 25000000:
-            st.error('エラー：ファイルが25MBを超えています。APIの上限により25MB以上のファイルは文字起こし不可です。ファイルを分割する等容量を少なくしてください', icon="🚨")
-        trans_start=st.button('文字起こし開始')
+            st.error('Error: The file exceeds 25MB. Due to API limitations, files over 25MB cannot be transcribed. Please reduce the file size by splitting it or similar methods.', icon="🚨")
+        trans_start=st.button('Start Transcription')
 
         if trans_start:
-            if not openai.api_key:
-                st.error('OpenAI API keyを入力してください。', icon="🚨")
-                st.stop()
-            else:
-                with st.spinner('***音声文字起こしを実行中です...***'):
-                    trans= openai.Audio.transcribe("whisper-1" ,upload_file)["text"]
-                st.success('***音声文字起こしを完了しました***')
-                st.write("***文字起こし結果***")
-                st.write(trans)
-                with st.spinner('***日本語の修正中です...***'):
-                    texts = japanese_spliter.split_text(trans)
-                    texts_modified=""
-                    for text in texts:            
-                        prompt=f"##音声文字起こしで不自然な文を削除し、自然な文章に修正してください。\n##音声文字起こし\n{text}\n##修正した文章\n"
-                        messages=[{"role": "system", "content": "あなたは優秀な日本語のエディターです。"},
-                            {"role": "user", "content": prompt}
-                        ]
-                        text_modified = openai.ChatCompletion.create(
-                            model="gpt-3.5-turbo",
-                            messages=messages,
-                            temperature=0,
-                        )
-                        texts_modified=texts_modified+text_modified["choices"][0]["message"]["content"]
-                st.success('***日本語の修正を完了しました***')
-                st.write("***文字起こし結果(修正後)***")
-                st.write(texts_modified)
+            with st.spinner('**Transcribing audio...**'):
+                trans = client.audio.transcriptions.create(model="whisper-1", file=upload_file).text
+            st.success('**Transcription completed**')
+            st.write("**Transcription Result**")
+            st.write(trans)
 
 if __name__ == "__main__":
     transcribe()
