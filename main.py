8c0b12804cacaca243816cfdc69abe1d9a9d5b93
tsuzukia21 <tsuzukia21@users.noreply.github.com>
2024-08-07 09:53:02 +0900

    add cookie

--- main.py
+++ main.py
@@ -2,19 +2,57 @@ import streamlit as st
 import streamlit_antd_components as sac
 import os
 from home import home
-# from st_chat_ChatGPT import chat
 from st_chat import st_chat
 from st_chat_Agent import agent
 from st_chat_vision import vision
 from st_MitoSheet import mito
 from st_transcribe import transcribe
 from st_rag_langgraph import st_rag_langgraph
+from streamlit_cookies_controller import CookieController
+import time
 
 st.set_page_config(layout="wide", page_title="tsuzukia's app")
 
-with st.sidebar.container():
+def update_index():
+    st.session_state.controller.set('index', st.session_state.index)
+
+if not hasattr(st.session_state, "openai_api_key"):
+    st.session_state.openai_api_key = os.getenv("OPENAI_API_KEY")
+if not hasattr(st.session_state, "anthropic_api_key"):
+    st.session_state.anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+if not hasattr(st.session_state, "google_api_key"):
+    st.session_state.google_api_key = os.getenv("GOOGLE_API_KEY")
+if not hasattr(st.session_state, "tavily_api_key"):
+    st.session_state.tavily_api_key = os.getenv("TAVILY_API_KEY")
+
+if not hasattr(st.session_state, "index"):
+    st.session_state.controller = CookieController()
+    cookies= st.session_state.controller.getAll()
+    time.sleep(0.1)
+    try:
+        st.session_state.index = cookies.get("index")
+    except AttributeError:
+        st.session_state.index = 1
+    if st.session_state.index is None:
+        st.session_state.index = 1
+    if st.session_state.index == 1:
+        st.session_state.menu = 'home'
+    elif st.session_state.index == 2:
+        st.session_state.menu = 'chat'
+    elif st.session_state.index == 3:
+        st.session_state.menu = 'agent'
+    elif st.session_state.index == 4:
+        st.session_state.menu = 'vision'
+    elif st.session_state.index == 5:
+        st.session_state.menu = 'transcribe'
+    elif st.session_state.index == 6:
+        st.session_state.menu = 'mitosheet'
+    elif st.session_state.index == 7:
+        st.session_state.menu = 'Adaptive RAG'
+
+with st.sidebar.container() as st.session_state.sidebar:
     editing = sac.Tag('editing', color='green')
-    menu = sac.menu([
+    sac.menu([
         sac.MenuItem('pages', type='group', children=[
         sac.MenuItem('home', icon='house-fill'),
         sac.MenuItem('chat', icon='emoji-smile-fill'),
@@ -29,17 +67,8 @@ with st.sidebar.container():
         sac.MenuItem('X', icon='twitter-x', href='https://twitter.com/tsuzukia_prgm'),
         sac.MenuItem('Zenn', icon='book', href='https://zenn.dev/tsuzukia')]),
         sac.MenuItem(type='divider'),
-    ],index=1)
+    ],index=st.session_state.index,key='menu',on_change=update_index)
 
-with st.sidebar:
-    if not hasattr(st.session_state, "openai_api_key"):
-        st.session_state.openai_api_key = os.getenv("OPENAI_API_KEY")
-    if not hasattr(st.session_state, "anthropic_api_key"):
-        st.session_state.anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
-    if not hasattr(st.session_state, "google_api_key"):
-        st.session_state.google_api_key = os.getenv("GOOGLE_API_KEY")
-    if not hasattr(st.session_state, "tavily_api_key"):
-        st.session_state.tavily_api_key = os.getenv("TAVILY_API_KEY")
     new_openai_api_key = st.text_input("OpenAI API Key", value = st.session_state.openai_api_key,type="password")
     new_anthropic_api_key = st.text_input("Anthropic API Key", value = st.session_state.anthropic_api_key, type="password")
     new_google_api_key = st.text_input("Google API Key", value = st.session_state.google_api_key,type="password")
@@ -51,17 +80,25 @@ with st.sidebar:
         st.session_state.google_api_key = new_google_api_key
         st.session_state.tavily_api_key = new_tavily_api_key
 
-if menu == 'home':
+if st.session_state.menu == 'home':
     home()
-elif menu == 'chat':
+    st.session_state.index = 1
+elif st.session_state.menu == 'chat':
     st_chat()
-elif menu == 'agent':
+    st.session_state.index = 2
+elif st.session_state.menu == 'agent':
     agent()
-elif menu == 'vision':
+    st.session_state.index = 3
+elif st.session_state.menu == 'vision':
     vision()
-elif menu == 'transcribe':
+    st.session_state.index = 4
+elif st.session_state.menu == 'transcribe':
     transcribe()
-elif menu == 'mitosheet':
+    st.session_state.index = 5
+elif st.session_state.menu == 'mitosheet':
     mito()
-elif menu == 'Adaptive RAG':
+    st.session_state.index = 6
+elif st.session_state.menu == 'Adaptive RAG':
     st_rag_langgraph()
+    st.session_state.index = 7
+    
